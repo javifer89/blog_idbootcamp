ca87be4f46b0b5060cad2d353a0ea1df2b37144e
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-06-17 00:22:43 +0200

    interfaz definida, creando funciones servicio

--- src/app/services/blog-post.service.ts
+++ src/app/services/blog-post.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../interfaces/post.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogPostService {
+arrPost : Post[] = []
 
-  constructor() { }
+
+  constructor() {
+  }
+
+
+  createPost(newPost: Post) : void{
+   this.arrPost.push(newPost);
+  }
+
+  getAll() :Post[] {
+  return this.arrPost;
+  }
+
+
+  getByCategory(category: string): Post {
+    let postSeleccionado: Post;
+    for (let post of this.arrPost) {
+      if (post.categoria === category) {
+        postSeleccionado = post;
+      }
+    }
+    return postSeleccionado;
+  }
+
+  getByCategory2(category: string): Post | undefined {
+  return this.arrPost.find(post => post.categoria === category )
+}
+
+
 }
