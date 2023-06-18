1740c18fd4ab7cc47d79a78d38e0f3af03be3cc2
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-06-18 12:59:51 +0200

    formulario eliminado y cambiado a Model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
-import { FormularioComponent } from './components/formulario/formulario.component';
+import { ModelComponent } from './components/model/model.component';
 
 const routes: Routes = [
   { path: 'posts', component: ListaPostsComponent },
-  { path: 'new', component: FormularioComponent}
+  { path: 'new', component: ModelComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
-import { FormularioComponent } from './components/formulario/formulario.component';
+
 import { ModelComponent } from './components/model/model.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -12,7 +12,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     ListaPostsComponent,
-    FormularioComponent,
     ModelComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
